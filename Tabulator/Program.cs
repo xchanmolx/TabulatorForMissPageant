acf448a56822da64c8089a9d4d0a2c053b7e43d7
xchanmolx <cmanigos@gmail.com>
2020-01-02 16:44:43 +0800

    Initial Commit.

--- Tabulator/Program.cs
+++ Tabulator/Program.cs
@@ -17,7 +17,7 @@ namespace Tabulator
         {   
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmLogin());  
         }
     }
 }
